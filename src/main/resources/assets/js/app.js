f3450c5993df25638d1d36963241a99ad78f2fa5
Francis Devereux <francis@bright-interactive.co.uk>
2015-05-29 14:50:25 +0100

    Track data in input elements in the controller so that it can be submitted

--- src/main/resources/assets/js/app.js
+++ src/main/resources/assets/js/app.js
@@ -1,7 +1,39 @@
 (function() {
     var app = angular.module('formDemo', []);
     app.controller('FormController', function() {
+        var form = this;
+        this.data = initialData();
         this.elements = elements;
+
+        // it would be better to construct a new set of objects instead of
+        // augmenting the JSON like this - only doing it this way because
+        // time is short!
+        for (var i = 0; i < this.elements.length; i++) {
+            var elementRow = this.elements[i];
+            for (var i = 0; i < elementRow.length; i++) {
+                var element = elementRow[i];
+                if (element && element.type == 'InputText') {
+                    elementRow[i] = {
+                        type: element.type,
+                        name: element.name,
+                        value: function(newValue) {
+                            if (arguments.length) {
+                                // set
+                                return (form.data[this.name] = newValue);
+                            } else {
+                                // get
+                                return form.data[this.name];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        this.submit = function() {
+            alert('data to submit: ' + this.data.name);
+            this.data = initialData()
+        }
     });
 
     app.directive('formElement', function() {
@@ -35,6 +67,10 @@
             templateUrl: 'directives/elements/button.html'
         };
     });
+
+    var initialData = function() {
+        return {name: 'Francis'};
+    };
 
     // some hard-coded data in the format we expect it to be returned from the server
     var elements = [
