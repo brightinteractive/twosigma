472ded00465008b966864600a26f773ed55d5fab
Francis Devereux <francis@bright-interactive.co.uk>
2015-05-29 12:58:01 +0100

    Extract out element functionality into directives

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -4,24 +4,48 @@
         this.elements = elements;
     });
 
+    app.directive('textElement', function() {
+        return {
+            restrict: 'E',
+            scope: {element: '='},
+            templateUrl: 'directives/elements/text.html'
+        };
+    });
+
+    app.directive('inputTextElement', function() {
+        return {
+            restrict: 'E',
+            scope: {element: '='},
+            templateUrl: 'directives/elements/input-text.html'
+        };
+    });
+
+    app.directive('buttonElement', function() {
+        return {
+            restrict: 'E',
+            scope: {element: '='},
+            templateUrl: 'directives/elements/button.html'
+        };
+    });
+
     // some hard-coded data in the format we expect it to be returned from the server
     var elements = [
         [
             {
-                "type": "Text",
-                "label": "What is your name?",
+                'type': 'Text',
+                'label': 'What is your name?',
             },
             {
-                "type": "InputText",
-                "name": "name",
+                'type': 'InputText',
+                'name': 'name',
             }
         ],
         [
             null,
             {
-                "type": "Button",
-                "name": "pressMe",
-                "label": "Press me!"
+                'type': 'Button',
+                'name': 'pressMe',
+                'label': 'Press me!'
             }
         ]
     ];
